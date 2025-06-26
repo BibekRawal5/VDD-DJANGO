01fe9387df19b8fa9045df749ea78a808a74e556
Bibek Rawal <bibek.rawal.588@gmail.com>
2025-06-26 17:04:30 +0545

    Remove model folder and add to .gitignore

--- VDD/urls.py
+++ VDD/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 # importing views from views..py
-from .views import home_view, result_view
+from .views import home_view, contact_view, about_view, result_view
 
 urlpatterns = [
-	path('', home_view ),
-	path('result.html', result_view)
+	path('', home_view, name='home'),
+	path('contact.html', contact_view, name='contact'),
+	path('about.html', about_view, name='about'),
+	path('result.html', result_view, name='result')
 ]
 
--- VDD/views.py
+++ VDD/views.py
@@ -1,13 +1,25 @@
+from tensorflow.python.util import deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+
 from django.shortcuts import render
 from .forms import ImageForm
 from .models import ImageModel
 import tensorflow as tf
 import numpy as np
+import tf_keras as k3
 from keras.utils import load_img, img_to_array
 
-model = tf.keras.models.load_model('VDD/static/model')
+
+model = k3.models.load_model('VDD/static/model')
 
 # Create your views here.
+def contact_view(request):
+    return render(request, "VDD/contact.html")
+
+
+def about_view(request):
+    return render(request, "VDD/about.html")
+
 def home_view(request):
     context ={}
     if request.method=="POST":
@@ -23,7 +35,7 @@ def home_view(request):
     else:
         form=ImageForm()
     context['form']=form
-    return render(request, "home.html", context)
+    return render(request, "VDD/home.html", context)
 
 def result_view(request, context):
     path = 'VDD/static/uploaded_images/'
@@ -37,13 +49,39 @@ def result_view(request, context):
     classes = model.predict(images, batch_size = 10)
     highest = 0
     c = 0
-    labels = sorted(['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy'])
+    labels = sorted([
+    'Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight', 
+    'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 
+    'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 
+    'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 
+    'Tomato__Target_Spot', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 
+    'Tomato__Tomato_mosaic_virus', 'Tomato_healthy'
+])
+    suggestion = [
+    "Apply copper-based bactericides and ensure proper field drainage to prevent Pepper Bacterial Spot.",
+    "No action needed, the plant is healthy.",
+    "Use fungicides containing chlorothalonil or mancozeb to treat Potato Early Blight.",
+    "Apply fungicides and practice crop rotation to manage Potato Late Blight.",
+    "No action needed, the plant is healthy.",
+    "Implement crop rotation and apply copper-based bactericides for Tomato Bacterial Spot.",
+    "Apply fungicides such as chlorothalonil or azoxystrobin to control Tomato Early Blight.",
+    "Use fungicides and remove infected leaves to control Tomato Late Blight.",
+    "Improve air circulation and use fungicides like sulfur-based sprays to treat Tomato Leaf Mold.",
+    "Apply fungicides and remove affected leaves to manage Tomato Septoria Leaf Spot.",
+    "Use miticides and increase humidity to control Tomato Spider Mites (Two-Spotted Spider Mite).",
+    "Apply fungicides and remove affected leaves to manage Tomato Target Spot.",
+    "Control whiteflies with insecticides and remove infected plants to manage Tomato Yellow Leaf Curl Virus.",
+    "Ensure proper sanitation and destroy infected plants to manage Tomato Mosaic Virus.",
+    "No action needed, the plant is healthy."
+]
+    print(classes, np.sum(classes[0]))
     for i,fn in enumerate(labels):
         if classes[0][i] > highest:
             highest = classes[0][i]
             c = i
 
     print(f"{path} is a {labels[c]}")
+    context['suggestion'] = suggestion[c]
     context['result'] = labels[c]
     context['path'] = 'uploaded_images/' + name
-    return render(request, 'result.html', context)
+    return render(request, 'VDD/result.html', context)
